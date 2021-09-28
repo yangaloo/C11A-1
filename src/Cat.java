12efe1e7d5de46c88b2f51a7a294811df1af4030
Zac Wilson <zac.53@live.com>
2021-09-28 11:32:57 -0600

    Changed eye color to cyan

--- src/Cat.java
+++ src/Cat.java
@@ -30,7 +30,7 @@ public class Cat {
 		g2.setColor(Color.blue);
 		g2.fillOval(x, y, HEAD_DIMENSION, HEAD_DIMENSION);
 		// Draw the eyes
-		g2.setColor(Color.green);
+		g2.setColor(Color.CYAN);
 		x = catX + EYE_X; 
 		y = catY + EYE_Y;
 		g2.fillOval(x, y, EYE_WIDTH, EYE_HEIGHT);
